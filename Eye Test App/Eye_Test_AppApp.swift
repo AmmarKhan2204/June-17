e4a1d3b87d3ebaf3e0921dcdb2978e8d908408e1
AmmarKhan2204 <133614685+AmmarKhan2204@users.noreply.github.com>
2024-05-23 09:29:36 -0700

    Initial Commit

---
+++ Eye Test App/Eye_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eye_Test_AppApp.swift
+//  Eye Test App
+//
+//  Created by Ammar Khan on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Eye_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
